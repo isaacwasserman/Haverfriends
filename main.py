2bc72ab3047389b2dcbed1b63575cd5ab1bea537
Tung Do <tdo@haverford.edu>
2021-02-02 14:05:00 -0500

    firebaes initialized in main.py

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from flask import *
+import firebase_admin 
+from firebase_admin import credentials, firestore, initialize_app
+
+#initialize firebase
+cred = credentials.Certificate("firebase/private-key.json") 
+default_app = initialize_app(cred)
+
+#create Database object
+db = firestore.client()
+
 app = Flask(__name__)       
 
 @app.route("/")  
@@ -7,6 +17,14 @@ def home():
 
 @app.route("/chat/<chatID>", methods = ["GET","POST"]) 
 def chat(chatID):
+    chatID=str(chatID)
+    chat_log= db.collection('chats').document(chatID).get()
+    if chat_log.exists:
+        log=[] 
+        
+        print(f'Document data: {chat_log.to_dict()}')
+    else:
+        print(u'No such document!')
     return render_template('chat.html')  
 
 @app.route("/chat", methods = ["GET","POST"]) 
@@ -34,10 +52,6 @@ def create_profile(user_ID):
 @app.route("/edit-profile/<user_ID>", methods = ["GET","POST"])
 def edit_profile(user_ID): 
     return render_template("edit_profile.html") 
-
-@app.route("/match-and-update")
-def match_and_update():
-    return render_template("home.html")
 
 if __name__ == '__main__': 
     app.run(debug=True) 
