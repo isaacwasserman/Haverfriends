610e67627c336aa49d7713220fe82ca40f66bc57
Tung Do <tdo@haverford.edu>
2021-02-02 15:47:20 -0500

    use isaac's functions

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 from flask import *
 import firebase_admin 
 from firebase_admin import credentials, firestore, initialize_app
-
-#initialize firebase
-cred = credentials.Certificate("firebase/private-key.json") 
-default_app = initialize_app(cred)
-
-#create Database object
-db = firestore.client()
+import firebase.firebaseFunctions as firebase_functions
 
 app = Flask(__name__)       
 
@@ -18,7 +12,7 @@ def home():
 @app.route("/chat/<chatID>", methods = ["GET","POST"]) 
 def chat(chatID):
     chatID=str(chatID)
-    messages= db.collection('chats').document(chatID).get().to_dict()['messages'] 
+    messages= firebase_functions.getChatConversation(chatID).to_dict()['messages'] 
     messages_array=[] 
     for message in messages: 
         #two tasks remaining: need to convert Firestore time object to Python string and 
@@ -27,7 +21,7 @@ def chat(chatID):
         complete_msg= sender + ": " + message['text']
         messages_array.append(complete_msg) 
     print(messages_array)
-    return render_template('chat.html', messages_array=messages_array)  
+    return render_template('chat.html', messages_array=messages_array, chatID=chatID)  
 
 @app.route("/chat", methods = ["GET","POST"]) 
 def chat_general(): 
