12f7d30e636fc9cae0cc2918701f282b51002c8c
Tung Do <tdo@haverford.edu>
2021-02-02 10:51:30 -0500

    add chat.html

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 @app.route("/")  
 def home():
     return render_template('home.html')
-    
+
+@app.route("/chat") #later need to add specific chatID to endpoint
+def chat(): 
+    return render_template('chat.html')  
+
 if __name__ == '__main__': 
     app.run(debug=True)
